6c49d5b7c7f048039899953274dfb72bf3c9fd25
ILoID <enric.carstens@gmail.com>
2023-05-18 13:27:38 +0200

    modal done, loading

--- components/Product.tsx
+++ components/Product.tsx
@@ -10,7 +10,7 @@ const Product: React.FC<ProductProps> = ({
     product,
 }) => {
     return (
-        <Link href={`/product/${product.id}`} className="h-96 flex flex-col p-5 rounded border group hover:scale-105 transition-transform ease-out duration-200" prefetch={false}>
+        <Link href={`/product/${product.id}`} className="h-96 flex flex-col p-5 rounded border group hover:scale-105 transition-transform ease-out duration-200">
             <div className="relative max-h-72 flex-1">
                 <ProductImage product={product} fill />
             </div>
